5c8121f37b189a0e1f5eeaa916041fbcdf6ddff5
lennox97 <lennox.samp@yahoo.com>
2018-06-25 23:38:14 +0300

    login and register base done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { sessions: 'sessions' }
+
   resources :songs
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to:'songs#index'
+
 
 end
